86856db2c9df9c983a275b9de4c488ecbca4c40c
TangenGP <TangenGP@gmail.com>
2016-07-07 03:19:55 +0200

    En videre oppgradering av Shoppen

---
+++ TGP_TEAM/Assets/Scripts/IfDead.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfDead : MonoBehaviour
+{
+	Health health;
+	ShopSystem shop;
+
+	void Start ()
+	{
+		health = GetComponent<Health>();
+		shop = GetComponent<ShopSystem>();
+	}
+	
+	void Update()
+	{
+		if (health.alive == false)
+		{
+			shop.Gold += 10;
+		}
+	}
+}
