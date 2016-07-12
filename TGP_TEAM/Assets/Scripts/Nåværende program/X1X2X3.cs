4f30e1c923910491ce43fe60ade32f9589f50a6c
TangenGP <TangenGP@gmail.com>
2016-07-12 09:30:30 +0200

    Videreutvikling av våpen og shoppen
    * Videre oppgradering av shoppen
    --> Kan nå oppgradere skade
    --> Kan kjøpe mer ammunisjon
    
    * Kan bytte mellom X1, X2 og X3
    --> Har egen ammunisjonholder for hver av skuddene, slik at man kan gå
    tom for ammunisjon på alle forskjellige typene

---
+++ TGP_TEAM/Assets/Scripts/Nåværende program/X1X2X3.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class X1X2X3 : MonoBehaviour {
+
+	public GameObject X1;
+	public GameObject X2;
+	public GameObject X3;
+
+	// Use this for initialization
+	void Start () {
+		X1.gameObject.SetActive(true);
+		X2.gameObject.SetActive(false);
+		X3.gameObject.SetActive(false);
+	}
+
+	public void X1damage()
+	{
+		X1.gameObject.SetActive(true);
+		X2.gameObject.SetActive(false);
+		X3.gameObject.SetActive(false);
+	}
+
+	public void X2damage()
+	{
+		X1.gameObject.SetActive(false);
+		X2.gameObject.SetActive(true);
+		X3.gameObject.SetActive(false);
+	}
+
+	public void X3damage()
+	{
+		X1.gameObject.SetActive(false);
+		X2.gameObject.SetActive(false);
+		X3.gameObject.SetActive(true);
+	}
+}
